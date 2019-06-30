d5d19b7acad1d33c119571f11d62ab46caaa31f7
Mateusz <226069@student.pwr.edu.pl>
2019-06-30 12:19:18 +0200

    Simple main activity with navigation drawer and ButterKnife added

--- app/src/main/java/com/mattech/outgoer/MainActivity.java
+++ app/src/main/java/com/mattech/outgoer/MainActivity.java
@@ -2,12 +2,29 @@ package com.mattech.outgoer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends Activity {
+    @BindView(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
+    @BindView(R.id.drawer)
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        navigationView.setNavigationItemSelectedListener((menuItem) -> {
+            menuItem.setChecked(true);
+            drawerLayout.closeDrawers();
+            return true;
+        });
     }
 }
