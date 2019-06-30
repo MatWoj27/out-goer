c7b858432d7c5bb0ce5ac5e7aaf119b4efd75c89
Mateusz <226069@student.pwr.edu.pl>
2019-06-30 15:25:03 +0200

    Animated background added to login activity layout

--- app/src/main/java/com/mattech/outgoer/LoginActivity.java
+++ app/src/main/java/com/mattech/outgoer/LoginActivity.java
@@ -1,13 +1,26 @@
 package com.mattech.outgoer;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends Activity {
+
+    @BindView(R.id.login_layout)
+    LinearLayout loginLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        AnimationDrawable animationDrawable = (AnimationDrawable) loginLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(3500);
+        animationDrawable.start();
     }
 }
