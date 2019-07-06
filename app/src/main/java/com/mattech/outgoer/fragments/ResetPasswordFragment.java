8603339672b5e0dce8eaedfaf0284dca80949808
Mateusz <226069@student.pwr.edu.pl>
2019-07-06 21:06:29 +0200

    Text animations for non edit text views

--- app/src/main/java/com/mattech/outgoer/fragments/ResetPasswordFragment.java
+++ app/src/main/java/com/mattech/outgoer/fragments/ResetPasswordFragment.java
@@ -1,6 +1,8 @@
 package com.mattech.outgoer.fragments;
 
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -16,6 +18,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ResetPasswordFragment extends Fragment {
+    private PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 0.5f, 1f);
+    private PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 0.5f, 1f);
+    private PropertyValuesHolder alpha = PropertyValuesHolder.ofFloat(View.ALPHA, 0f, 1f);
 
     @BindView(R.id.reset_header)
     TextView resetHeader;
@@ -37,17 +42,28 @@ public class ResetPasswordFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_reset_password, container, false);
         ButterKnife.bind(this, view);
-        sendBtn.setOnClickListener(v -> {
-            resetHeader.setText(getResources().getString(R.string.enter_reset_code));
-            resetInfo.setText(getResources().getString(R.string.reset_code_info));
-            mail.setHint(getResources().getString(R.string.reset_code_hint));
-            sendBtn.setText(R.string.verify);
-            sendBtn.setOnClickListener(vi -> {
-                newPass.setVisibility(View.VISIBLE);
-                mail.setHint(R.string.confirm_new_pass);
-                sendBtn.setText(R.string.change);
-            });
-        });
+        sendBtn.setOnClickListener(v -> arrangeResetCodeStep());
         return view;
+    }
+
+    private void animateTextChange(TextView textView, String newText) {
+        ObjectAnimator.ofPropertyValuesHolder(textView, scaleX, scaleY, alpha).setDuration(300).start();
+        textView.setText(newText);
+    }
+
+    private void arrangeResetCodeStep() {
+        animateTextChange(resetHeader, getResources().getString(R.string.enter_reset_code));
+        animateTextChange(resetInfo, getResources().getString(R.string.reset_code_info));
+        mail.setHint(getResources().getString(R.string.reset_code_hint));
+        animateTextChange(sendBtn, getResources().getString(R.string.verify));
+        sendBtn.setOnClickListener(v -> arrangeChangePassStep());
+    }
+
+    private void arrangeChangePassStep() {
+        animateTextChange(resetHeader, getResources().getString(R.string.reset_pass));
+        animateTextChange(resetInfo, getResources().getString(R.string.reset_pass_info));
+        newPass.setVisibility(View.VISIBLE);
+        mail.setHint(R.string.confirm_new_pass);
+        animateTextChange(sendBtn, getResources().getString(R.string.change));
     }
 }
